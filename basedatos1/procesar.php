b552ac0a68b55734c0fb31eeaa6cf5af9a5a1a74
OswaldoGSDAW <147555767+OswaldoGSDAW@users.noreply.github.com>
2024-04-26 08:58:33 +0200

    Ejercicio base de datos He creado una condicón si ya existe el usuario con DNI recoge error y lo muestra

--- basedatos1/procesar.php
+++ basedatos1/procesar.php
@@ -1,36 +1,43 @@
 <?php
-    require_once("./libs/bGeneral.php");
-    require_once("./libs/funciones.php");
-    require_once("./libs/conexion.php");
+require_once("./libs/bGeneral.php");
+require_once("./libs/funciones.php");
+require_once("./libs/conexion.php");
 
-    $errores = [];
+$errores = [];
 
-    if(!isset($_REQUEST["enviar"])){
-        include("formulario.php");
-    } else {
+if (!isset($_REQUEST["enviar"])) {
+    include("formulario.php");
+} else {
 
-        //Recoger datos del formulario
-        $nombre = recoge("nombre");
-        $apellido = recoge("apellido");
-        $dni = recoge("dni");
-        
-        cUser($nombre, 'nombre', $errores);
-        cUser($apellido, 'apellido', $errores);
-        cUser($dni, 'dni', $errores,9,9);
-        
-        if(empty($errores)){
-            $sqlInsertar = "INSERT INTO usuarios (nombre, apellido, dni) VALUES ('$nombre', '$apellido', '$dni')";
-            if(mysqli_query($conexion, $sqlInsertar)){
+    //Recoger datos del formulario
+    $nombre = recoge("nombre");
+    $apellido = recoge("apellido");
+    $dni = recoge("dni");
+
+    cUser($nombre, 'nombre', $errores);
+    cUser($apellido, 'apellido', $errores);
+    cUser($dni, 'dni', $errores, 9, 9);
+
+    if (empty($errores)) {
+        $sqlInsertar = "INSERT INTO usuarios (nombre, apellido, dni) VALUES ('$nombre', '$apellido', '$dni')";
+        //Verificar si el DNI si ya existe
+        $sqlVerificarDNI = "SELECT dni FROM usuarios WHERE dni = '$dni'";
+        $resultado = mysqli_query($conexion, $sqlVerificarDNI);
+
+        if (mysqli_num_rows($resultado) > 0) {
+            //El DNI ya existe
+            echo "Ya existe usuario con ese DNI";
+        } else {
+
+            if (mysqli_query($conexion, $sqlInsertar)) {
                 echo "Registros insertados con éxito";
+                header("Location:destino.php?nombre=$nombre&apellido=$apellido&dni=$dni");
             } else {
                 echo "Error al insertar registros: " . mysqli_error($conexion);
             }
-            mysqli_close($conexion);
-            header("Location=destino.php?nombre=$nombre&apellido=$apellido&dni=$dni");
-        } else {
-        include("formulario.php");
         }
+        mysqli_close($conexion);
+    } else {
+        include("formulario.php");
     }
-
-
-?>
+}
