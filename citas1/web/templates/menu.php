118055c91c04da3247c9fe0cbc66f9fc6b31d627
OswaldoGSDAW <147555767+OswaldoGSDAW@users.noreply.github.com>
2024-06-02 11:07:51 +0200

    Creo en templates, menu y menu usuario

---
+++ citas1/web/templates/menu.php
@@ -0,0 +1,9 @@
+<!-- menu.php -->
+<nav>
+    <ul>
+        <li><a href="index.php?ctl=inicio">Página Inicio</a></li>
+        <li><a href="index.php?ctl=login">Inicio de sesión</a></li>
+        <li><a href="index.php?ctl=register">Registrarse</a></li>
+        <li><a href="index.php?ctl=citasPublicas">Ver citas públicas</a></li>
+    </ul>
+</nav>
---
+++ citas1/web/templates/menu_usuario.php
@@ -0,0 +1,9 @@
+<!-- menu_usuario.php -->
+<nav>
+    <ul>
+        <li><a href="index.php?ctl=home">Página Inicio</a></li>
+        <li><a href="index.php?ctl=cerrarSesion">Cerrar sesión</a></li>
+        <li><a href="index.php?ctl=addCitas">Añadir Citas</a></li>
+        <li><a href="index.php?ctl=modCitas">Modificar Citas</a></li>
+    </ul>
+</nav>
